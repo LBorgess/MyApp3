632d5bb7721624c138bc927d61773ddc9393fc95
LBorgess <lucas_rancer@hotmail.com>
2023-04-06 21:24:58 -0300

    menu-bottom-navigation item and cases

--- app/src/main/java/br/com/etecia/myapp/BottomNavigationActivity.java
+++ app/src/main/java/br/com/etecia/myapp/BottomNavigationActivity.java
@@ -1,14 +1,46 @@
 package br.com.etecia.myapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class BottomNavigationActivity extends AppCompatActivity {
+    BottomNavigationView idBottomNavView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bottom_navigation_layout);
+
+        idBottomNavView = findViewById(R.id.idBottomNavView);
+
+        idBottomNavView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.mNavFav:
+                        Toast.makeText(getApplicationContext(), "Casada", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.mNavMusic:
+                        Toast.makeText(getApplicationContext(), "Funk", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.mNavPlace:
+                        Toast.makeText(getApplicationContext(), "Achouuu", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.mNavNewspaper:
+                        Toast.makeText(getApplicationContext(), "Boa noite.", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
